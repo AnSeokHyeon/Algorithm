0aca17e22b7b2bd898997e200088304314148709
AnSeokHyeon <30994764+AnSeokHyeon@users.noreply.github.com>
2021-01-27 21:17:45 +0900

    Update Boj1316.java

--- src/algo/Boj1316.java
+++ src/algo/Boj1316.java
@@ -11,28 +11,32 @@ public class Boj1316 {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		int cnt = 0;
+		
 		while((T--)>0) {
 
 			String s = sc.next();
-			boolean[] chk = new boolean[26];
-			boolean chk2 = true;
-			for (int i = 0; i < s.length(); i++) {
+			boolean[] chk = new boolean[26]; // 알파벳 나왔었는지 체크 용도
+			boolean chk2 = true; // 그룹단어 체크 용도
+			
+			for (int i = 0; i < s.length(); i++) 
+			{
 				int n = s.charAt(i) - 'a';
-
-				if (chk[n]) {
-					if (!(s.charAt(i - 1) == s.charAt(i))) {
+				
+				if (chk[n]) 
+				{
+					if (!(s.charAt(i - 1) == s.charAt(i))) 
+					{
 						chk2 = false;
-
 						break;
-
 					}
-				} else
-					chk[n] = true;
-
+				} 
+				else{
+					chk[n] = true;	
+				}
 			}
 			if(chk2) cnt++;
-
 		}
+		
 		System.out.println(cnt);
 		
 	}
