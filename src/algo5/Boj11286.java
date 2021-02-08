bcc4109362fa43b62beeb2feeb358bf113eda109
AnSeokHyeon <the99331122@gmail.com>
2021-02-08 19:58:47 +0900

    2021년 2월 8일

---
+++ src/algo5/Boj11286.java
@@ -0,0 +1,56 @@
+package algo5;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Boj11286 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		PriorityQueue<Integer> q = new PriorityQueue<>(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				// TODO Auto-generated method stub
+				if (Math.abs(o1) == Math.abs(o2))
+					return o1 - o2;
+				return Math.abs(o1) - Math.abs(o2);
+			}
+		});
+
+		StringBuilder sb = new StringBuilder();
+
+		while (N-- > 0) {
+			int x = Integer.parseInt(br.readLine());
+			if (x == 0) {
+				if(q.size() > 0) {
+
+					sb.append(q.poll() + "\n");
+	
+				}
+				else {
+					sb.append(0 + "\n");
+				}
+			} else {
+
+				q.add(x);
+
+			}
+
+		}
+
+		bw.write(sb.toString());
+		bw.close();
+		br.close();
+	}
+
+}
