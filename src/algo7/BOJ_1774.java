522b9eb93b3b50b15d8f9508ca2e666182cd072c
AnSeokHyeon <the99331122@gmail.com>
2021-03-21 01:02:02 +0900

    2021년 3월 21일 커밋

---
+++ src/algo7/BOJ_1774.java
@@ -0,0 +1,121 @@
+package algo7;
+
+
+// 1774 우주신과의 교감
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ_1774 {
+	static int p[];
+
+	static int find(int n) {
+		if (p[n] == n)
+			return n;
+
+		return p[n] = find(p[n]);
+
+	}
+
+	static boolean union(int a, int b) {
+		int pa = find(a);
+		int pb = find(b);
+
+		if (pa == pb)
+			return false;
+
+		if (pa > pb)
+			p[pa] = pb;
+		else
+			p[pb] = pa;
+		return true;
+	}
+	
+	static class Node{
+		int x;
+		int y;
+		double w;
+		public Node(int x, int y, double w) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.w = w;
+		}
+		
+		
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer input = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(input.nextToken());
+		int M = Integer.parseInt(input.nextToken());
+		p = new int[N + 1];
+		
+		double x[] = new double[N+1];
+		double y[] = new double[N+1];
+		for(int i= 1; i < N+1; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			
+			x[i] = Double.parseDouble(st.nextToken());
+			y[i] = Double.parseDouble(st.nextToken());
+		}
+
+
+		for (int i = 0; i < N + 1; i++) {
+			p[i] = i;
+		}
+		
+		while(M-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			union(a, b);
+			
+		}
+		
+
+		PriorityQueue<Node> q = new PriorityQueue<>(new Comparator<Node>() {
+
+			@Override
+			public int compare(Node o1, Node o2) {
+				// TODO Auto-generated method stub
+				return (int)((o1.w - o2.w)*100);
+			}
+		});
+		
+		for(int i = 1; i<N+1; i++) {
+			for(int j = i+1; j<N+1;j++) {
+				double X = Math.abs(x[i] - x[j]);
+				double Y = Math.abs(y[i] - y[j]);
+				double X2 = Math.pow(X, 2);
+				double Y2 = Math.pow(Y, 2);
+				double w = Math.sqrt(X2 + Y2);
+				q.add(new Node(i, j, w));
+			}
+		}
+
+		
+		double result = 0;
+		while (!q.isEmpty()) {
+			Node p = q.poll();
+			int now = p.x;
+			int next = p.y;
+			double w = p.w;
+			if (!union(now, next)) continue;
+			result += w;
+		}	
+
+		System.out.printf("%.2f", result);
+		br.close();
+		bw.close();
+	}
+
+}
